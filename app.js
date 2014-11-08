1d998929c23928c7a6bcd3d5be0b39e1451e556e
Chris James Martin <cj@cjmart.in>
2014-11-07 23:50:55 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+var express = require('express'),
+    path = require('path'),
+    favicon = require('static-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    crypto = require('crypto'),
+    exphbs  = require('express-handlebars'),
+    hbsHelpers = require('./lib/hbs-helpers');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main', helpers: hbsHelpers}));
+app.set('view engine', '.hbs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    var id;
+    if (typeof req.id === 'undefined') {
+        id = crypto.randomBytes(16).toString('hex');
+        req.id = id.substring(0,8);
+    }
+    next();
+});
+
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,87 @@
+var _ = require('underscore'),
+	express = require('express'),
+	router = express.Router(),
+	flickrAPI = require('../lib/flickr-api');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	var setsCall = {
+		method: 'flickr.photosets.getList',
+		auth: false,
+		params: {
+			user_id: '43813659@N03',
+			primary_photo_extras: 'url_q,url_n,url_z,url_l,url_h,url_k'
+		}
+	};
+
+	flickrAPI.get(req, setsCall.method, setsCall.params, setsCall.auth, function(err, responseData) {
+		if (err) {
+			console.log("Flickr API error: " + err);
+			res.send(500);
+		}
+
+		console.log(responseData);
+		var photosets = [];
+		_.each(responseData.photosets.photoset, function(s){
+			if (s.primary_photo_extras.url_l) {
+				photosets.push({
+					id: s.id,
+					title: s.title._content,
+					images: {
+						large: s.primary_photo_extras.url_l,
+						medium: s.primary_photo_extras.url_z,
+						small: s.primary_photo_extras.url_n,
+						square: s.primary_photo_extras.url_q
+					}
+				});
+			}
+		});
+
+		console.log(photosets);
+		res.render('index', { title: 'Gallery', photosets: photosets });
+	});
+});
+
+router.get('/:set_id', function(req, res) {
+	var setCall = {
+		method: 'flickr.photosets.getPhotos',
+		auth: false,
+		params: {
+			extras: 'url_q,url_n,url_z,url_l,url_h,url_k,media',
+			page: 1,
+			per_page: 500,
+			photoset_id: req.params.set_id
+		}
+	};
+
+	flickrAPI.get(req, setCall.method, setCall.params, setCall.auth, function(err, responseData) {
+		if (err) {
+			console.log("Flickr API error: " + err);
+			res.send(500);
+		}
+
+		console.log(responseData);
+		var photos = [];
+		// _.each(responseData.photosets.photoset, function(s){
+		// 	if (s.primary_photo_extras.url_l) {
+		// 		photosets.push({
+		// 			id: s.id,
+		// 			title: s.title._content,
+		// 			images: {
+		// 				large: s.primary_photo_extras.url_l,
+		// 				medium: s.primary_photo_extras.url_z,
+		// 				small: s.primary_photo_extras.url_n,
+		// 				square: s.primary_photo_extras.url_q
+		// 			}
+		// 		});
+		// 	}
+		// });
+
+		console.log(photos);
+		res.render('index', { title: 'Gallery', photos: photos });
+	});
+
+	// res.render('photoset', {id: req.params.set_id});
+});
+
+module.exports = router;
