e65d028b6caa90e0ea4b3ae77bbb276569fa65d4
Chris James Martin <cj@cjmart.in>
2014-11-10 16:54:58 -0800

    add caching, always cache photosets

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var express = require('express'),
+var _ = require('underscore'),
+	express = require('express'),
     path = require('path'),
     favicon = require('static-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     crypto = require('crypto'),
-    exphbs  = require('express-handlebars'),
-    hbsHelpers = require('./lib/hbs-helpers');
+    exphbs = require('express-handlebars'),
+    slug = require('slug'),
+    hbsHelpers = require('./lib/hbs-helpers'),
+    flickrAPI = require('./lib/flickr-api'),
+	utils = require('./lib/utilities'),
+	cache = require('./lib/cache');
 
 var routes = require('./routes/index');
 
@@ -30,6 +35,68 @@ app.use(function(req, res, next) {
         req.id = id.substring(0,8);
     }
     next();
+});
+
+// We always need the photosets in memcache, since we're using slugs in urls and need a lookup.
+// So, if they're stale, re-fetch them, otherwise pull from cache and attach to the req.
+app.use(function(req, res, next) {
+	var apiCall = {
+		method: 'flickr.photosets.getList',
+		auth: false,
+		params: {
+			user_id: '43813659@N03',
+			primary_photo_extras: 'url_q,url_n,url_z,url_l,url_h,url_k'
+		}
+	};
+
+	var cacheTime = 60*60; // 1 Hour cache. 
+	var cacheKey = 'photosets_' + crypto.createHash('md5').update("GET" + apiCall.method + JSON.stringify(apiCall.args)).digest("hex");
+
+	if (apiCall.auth && req.user_token) { // If we want this call to be auth'd and we have a user token, add the token to the cache key for uniqueness.
+		cacheKey += '_' + req.user_token;
+	}
+
+	cache.get(cacheKey, function(err, cacheData) {
+		// Cache error.
+		if (err) {
+			console.error('[' + req.id + ']', '[cache] Cache.get error: ', err);
+		}
+
+		// Cache miss.
+		if (!cacheData) {
+			flickrAPI.get(req, apiCall.method, apiCall.params, apiCall.auth, function(err, responseData) {
+				if (err) {
+					console.log("Flickr API error: " + err);
+					return res.send(500);
+				}
+				
+				var photosets = {};
+				_.each(responseData.photosets.photoset, function(s){
+					photosets[slug(s.title._content).toLowerCase()] = {
+						id: s.id,
+						title: s.title._content,
+						images: utils.normalizePhotoSizes(s.primary_photo_extras)
+					};
+				});
+
+				cache.set(cacheKey, photosets, cacheTime, function(err) {
+					if (err) {
+						console.error('[' + req.id + ']', '[cache] Trying to cache value sized: ', Buffer.byteLength(JSON.stringify(photosets), 'utf8'));
+						console.error('[' + req.id + ']', '[cache] Cache.set error: ', err);
+					}
+				});
+
+				req.photosets = photosets;
+				next();
+			});
+
+		// Cache hit.
+		} else {
+			console.log('[' + req.id + ']', '[cache]', 'using cached API response method=' + apiCall.method, 'key=' + cacheKey);
+			req.photosets = cacheData;
+			next();
+		}
+	});
 });
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -2,46 +2,20 @@ var _ = require('underscore'),
 	express = require('express'),
 	router = express.Router(),
 	flickrAPI = require('../lib/flickr-api'),
-	utils = require('../lib/utilities');
+	utils = require('../lib/utilities'),
+	cache = require('../lib/cache'),
+	crypto = require('crypto');
 
 /* GET home page. */
 router.get('/gallery', function(req, res) {
-	var setsCall = {
-		method: 'flickr.photosets.getList',
-		auth: false,
-		params: {
-			user_id: '43813659@N03',
-			primary_photo_extras: 'url_q,url_n,url_z,url_l,url_h,url_k'
-		}
-	};
-
-	flickrAPI.get(req, setsCall.method, setsCall.params, setsCall.auth, function(err, responseData) {
-		if (err) {
-			console.log("Flickr API error: " + err);
-			return res.send(500);
-		}
-
-		console.log(responseData);
-		var photosets = [];
-		_.each(responseData.photosets.photoset, function(s){
-			if (s.primary_photo_extras.url_l) {
-				photosets.push({
-					id: s.id,
-					title: s.title._content,
-					images: utils.normalizePhotoSizes(s.primary_photo_extras)
-				});
-			}
-		});
-
-		console.log(photosets);
-		res.render('index', { title: 'Gallery', photosets: photosets });
-	});
+	res.render('index', { title: 'Gallery', photosets: req.photosets });
 });
 
 router.get('/gallery/:set_id', function(req, res) {
 	var setCall = {
 		method: 'flickr.photosets.getPhotos',
 		auth: false,
+		cachetime: 60*60, // 1 Hour cache. 
 		params: {
 			extras: 'url_q,url_n,url_z,url_l,url_h,url_k,media',
 			page: 1,
@@ -61,10 +35,8 @@ router.get('/gallery/:set_id', function(req, res) {
 			return res.send(404);
 		}
 
-		console.log(responseData);
 		var photos = utils.photosFromList(responseData.photoset.photo);
 
-		console.log(photos);
 		res.render('photoset', { title: 'Gallery', photos: photos });
 	});
 });
